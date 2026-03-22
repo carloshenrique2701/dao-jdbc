fe8b18882883a4ad3f4349d2d5998f9287b3b7a9
Carlos Henrique Rodrigues Ribeiro <carloshrr1209@gmail.com>
2026-03-22 13:18:52 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
@@ -13,8 +13,10 @@ public class Seller implements Serializable {
 	private Date birthDate;
 	private Double baseSalary;
 	private Department department;
+
 	public Seller() {
 	}
+
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
@@ -23,46 +25,64 @@ public class Seller implements Serializable {
 		this.baseSalary = baseSalary;
 		this.department = department;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+
 	public Double getBaseSalary() {
 		return baseSalary;
 	}
+
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+
 	public Department getDepartment() {
 		return department;
 	}
+
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -74,13 +94,11 @@ public class Seller implements Serializable {
 		Seller other = (Seller) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	@Override
 	public String toString() {
-		return "Seller id: " + id + " | name: " + name +
-				" | email: " + email + " | birthDate: " + birthDate + 
-				" | baseSalary: " + baseSalary + " | department: " + department;
+		return "Seller id: " + id + " | name: " + name + " | email: " + email + " | birthDate: " + birthDate
+				+ " | baseSalary: " + baseSalary + " | department: " + department;
 	}
-	
-	
 
 }
